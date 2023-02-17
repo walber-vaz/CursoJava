2009a0d5f95957e9ad368cc3a8cc1e1c7413a4b6
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-17 09:21:26 -0300

    feat: commentary in code java(java-doc, multi-lineand inline)

---
+++ src/introducao/Comentarios.java
@@ -0,0 +1,26 @@
+package introducao;
+
+/**
+ * Comentários em Java
+ * 
+ * @author Walber Vaz
+ * @version 1.0
+ * @since 2023-02-17
+ */
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentário de uma linha
+        // Use comentário com moderação
+        System.out.println("Linha 1");
+        System.out.println("Linha 2");
+        System.out.println("Linha 3");
+        /*
+         * Comentário de
+         * várias linhas
+         */
+        System.out.println("Linha 4");
+        System.out.println("Linha 5");
+        System.out.println("Linha 6");
+    }
+}
