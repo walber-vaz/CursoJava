a2cfee63698ddad3a797ad01722dff279a104912
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-18 15:16:10 -0300

    feat: estruture conditional in java (operador ternario)

---
+++ src/introducao/OperadorTernario.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        double media = 7;
+        String resultadoFinal = media >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println("Aluno esta: " + resultadoFinal);
+    }
+}
